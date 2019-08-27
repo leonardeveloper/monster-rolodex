65884330f597b6a46fcd204eaaf0eca2cda82868
leonardeveloper <6325887+leonardeveloper@users.noreply.github.com>
2019-08-27 22:34:05 +0800

    Updates

---
+++ precache-manifest.27533cb8117ce8422c864e3c8c929374.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eada5afb0a5cf75d070685671814ba6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "55a685c7c42a67b2bd49",
+    "url": "/monster-rolodex/static/css/main.083cdc85.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monster-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "55a685c7c42a67b2bd49",
+    "url": "/monster-rolodex/static/js/main.860e6e1b.chunk.js"
+  },
+  {
+    "revision": "1f1fdb189afd82863836",
+    "url": "/monster-rolodex/static/js/runtime~main.80659f76.js"
+  }
+]);
